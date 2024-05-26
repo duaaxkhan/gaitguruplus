cc043a43b51cb05e34a2d45ca2e26fa82008718a
Duaa <duaashahidkhan01@gmail.com>
2024-05-26 12:31:31 +0500

    Commit:changed Dplot

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,27 @@
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCTdKjIkF944O3LyAJxqgVk_noXSfl_ayg",
+//   authDomain: "gaitguru-aa584.firebaseapp.com",
+//   projectId: "gaitguru-aa584",
+//   storageBucket: "gaitguru-aa584.appspot.com",
+//   messagingSenderId: "507716371847",
+//   appId: "1:507716371847:web:cb899e2be4ba901b9dff22"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export default app;
+
+// firebase.js or firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCTdKjIkF944O3LyAJxqgVk_noXSfl_ayg",
   authDomain: "gaitguru-aa584.firebaseapp.com",
@@ -14,5 +33,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
-export default app;
+export { app, auth, storage };
